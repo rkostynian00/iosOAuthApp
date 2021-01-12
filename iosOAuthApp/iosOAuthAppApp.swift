34d72f1ac057c0c1129dcb5817bf7c6d8b2feb30
Rostyslav Kostynian <kostinyan16@gmail.com>
2021-01-12 17:52:14 +0200

    Initial Commit

---
+++ iosOAuthApp/iosOAuthAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosOAuthAppApp.swift
+//  iosOAuthApp
+//
+//  Created by Rostyslav Kostynian on 12.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct iosOAuthAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
